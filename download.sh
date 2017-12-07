1b3df819a395ee3503af5f987da2e6ec58916956
Ishu Dharmendra Garg <ishugarg567@gmail.com>
2017-12-07 17:49:41 +0530

    Delete download.sh

--- download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/dlslam/vis/$MODEL vis/
-rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/dlslam/*jpg .
-
-
-# rsync -vaP -e "ssh" matrix.ml.cmu.edu:~/dlslam/rcnn .
